e08becb042b13ee518019c2a23ed0795dc600e71
ZARIFMAC <uzbzarif@gmail.com>
2022-09-29 18:17:16 +0500

    check status code of server

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
-def home(x):
-    pass 
+#Import reqests module
+import requests
+#Import json module
+import json
 
+#Define the main function
+def main():
+    #Define the localhost URL
+    url = "http://localhost:8000/"
+    #Check the status code of the URL
+    r = requests.get(url)
+    #Print the status code
+    print(r.status_code)
 
-home(1,2)
+#Run the main function
+if __name__ == "__main__":
+    main()
