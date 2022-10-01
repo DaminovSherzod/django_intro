35bed7f654966b2d34474b4837cca9a3f97947a3
ZARIFMAC <uzbzarif@gmail.com>
2022-10-01 15:19:07 +0500

    myapp

---
+++ myapp/views.py
@@ -0,0 +1,21 @@
+from django.http import HttpResponse
+
+def index(request):
+  
+    return HttpResponse("<h1>Index</h1><h3>Index page</h3>")
+
+def home(request):
+      
+     return HttpResponse("<h1>Home</h1>")
+
+def about(request):
+      
+     return HttpResponse("<h1>About</h1>")  
+
+def contact(request): 
+    return HttpResponse("<h1>Contact</h1>")
+
+
+
+    
+
