d0e53d381067078217a27b1c068b985c54dfd7d2
ZARIFMAC <uzbzarif@gmail.com>
2022-10-01 14:58:31 +0500

    index home about contact

--- intro/view.py
+++ intro/view.py
@@ -15,23 +15,6 @@ def about(request):
 def contact(request): 
     return HttpResponse("<h1>Contact</h1>")
 
-def services(request):
-    return HttpResponse("<h1>Services</h1>")
-
-def blog(request):
-    return HttpResponse("<h1>Blog</h1>")
-
-def portfolio(request):
-    return HttpResponse("<h1>Portfolio</h1>")
-
-def team(request):
-    return HttpResponse("<h1>Team</h1>")
-
-def faq(request):
-    return HttpResponse("<h1>FAQ</h1>")
-
-def error(request):
-    return HttpResponse("<h1>Error</h1>")
 
 
     
