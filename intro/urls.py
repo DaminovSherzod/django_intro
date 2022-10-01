d2a673a6d3ede461b9fdb9990f2d132836f99ad7
ZARIFMAC <uzbzarif@gmail.com>
2022-10-01 15:02:24 +0500

    path

--- intro/urls.py
+++ intro/urls.py
@@ -1,7 +1,10 @@
 
 from django.urls import path
-from .view import home
+from .view import home, about, contact, index
 
 urlpatterns = [
     path('', home),
+    path('about/', about),
+    path('contact/', contact),
+    path('index/', index),
 ]
