ffb141b452f21fe287130a3b773fbdcf3d1a6c06
ZARIFMAC <uzbzarif@gmail.com>
2022-10-06 17:44:25 +0500

    add admin url

--- intro/urls.py
+++ intro/urls.py
@@ -1,7 +1,9 @@
 
 from django.urls import path
 from myapp.views import index, home, about, contact
+from django.contrib import admin
 urlpatterns = [
+    path('admin/',admin.site.urls),
     path('', home),
     path('about/', about),
     path('contact/', contact),
